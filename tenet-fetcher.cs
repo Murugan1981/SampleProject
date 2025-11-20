d391b6a9b00d37e0c6de5e06bbd6b44c1d61d97e
Murugan1981 <Muruganishere@gmail.com>
2025-11-20 09:51:56 +0530

    Update tenet-fetcher.cs

--- tenet-fetcher.cs
+++ tenet-fetcher.cs
@@ -1,54 +1,27 @@
 #nullable enable
 
-using System.IO;
-using System.Linq;
-using System.Collections.Generic;
+using System;
+using System.Threading.Tasks;
 using OfficeOpenXml;
-using TenantExtractor.Models;
-// Optional alias to avoid any LicenseContext ambiguity:
-// using EPPLicenseContext = OfficeOpenXml.LicenseContext;
+using TenantExtractor.Services;
 
-namespace TenantExtractor.Services
+namespace TenantExtractor
 {
-    public static class ExcelWriter
+    internal static class Program
     {
-        public static void Save(List<TenantRecord> prd, List<TenantRecord> uat, string rawPath)
+        private static async Task Main(string[] args)
         {
-            // Either keep the fully qualified form:
-            OfficeOpenXml.ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
-            // Or, if you used the alias above:
-            // ExcelPackage.LicenseContext = EPPLicenseContext.NonCommercial;
-
-            string excelPath = Path.Combine(rawPath, "tenant_data.xlsx");
-
-            using (var package = new ExcelPackage())
+            // ✔ EPPlus 8 — Correct License Configuration
+            ExcelPackage.License = new ExcelLicense
             {
-                var prdSheet = package.Workbook.Worksheets.Add("PRD");
-                var uatSheet = package.Workbook.Worksheets.Add("UAT");
+                LicenseContext = LicenseContext.NonCommercial
+            };
 
-                WriteSheet(prdSheet, prd);
-                WriteSheet(uatSheet, uat);
+            Console.WriteLine("Executing Tenant Extractor...");
+            var fetcher = new TenantFetcher();
 
-                package.SaveAs(new FileInfo(excelPath));
-            }
-        }
-
-        private static void WriteSheet(OfficeOpenXml.ExcelWorksheet sheet, List<TenantRecord> data)
-        {
-            var flattened = data.Select(TenantFlattener.Flatten).ToList();
-            var headers = flattened.SelectMany(x => x.Keys).Distinct().ToList();
-
-            for (int col = 0; col < headers.Count; col++)
-                sheet.Cells[1, col + 1].Value = headers[col];
-
-            for (int row = 0; row < flattened.Count; row++)
-            {
-                for (int col = 0; col < headers.Count; col++)
-                {
-                    flattened[row].TryGetValue(headers[col], out var value);
-                    sheet.Cells[row + 2, col + 1].Value = value;
-                }
-            }
+            await fetcher.Run();
+            Console.WriteLine("Completed");
         }
     }
 }
